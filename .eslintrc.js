3dc85e02eba1992a33aca2bc5103ab99f72da996
Prajwal264 <prajwalp264@gmail.com>
2021-05-14 20:05:52 +0530

    testing eslint

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2020': true,
+  },
+  'extends': [
+    'airbnb',
+  ],
+  'parser': 'babel-eslint',
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true,
+    },
+    'ecmaVersion': 11,
+    'sourceType': 'module',
+  },
+  'plugins': [
+    'react',
+  ],
+  'rules': {
+    'no-invalid-this': 0,
+    'new-cap': 0,
+  },
+};
