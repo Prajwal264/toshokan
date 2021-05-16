3c79f55b0797ac0246fa279a999c11d8171f7924
Prajwal264 <prajwalp264@gmail.com>
2021-05-16 15:42:56 +0530

    jobs screen added

--- src/pages/admin/jobs.jsx
+++ src/pages/admin/jobs.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
+import { Nav, Tab } from 'react-bootstrap';
+import * as styles from '../../styles/pages/jobs.module.scss';
 import AppLayout from '../../components/app-layout';
+import JobsTab from '../../components/jobs-tab';
 
 /**
  *
@@ -9,7 +13,50 @@ import AppLayout from '../../components/app-layout';
 function Jobs() {
   return (
     <AppLayout>
-      <h2>Jobs</h2>
+      <div className={styles.jobsWrapper}>
+        <div className={styles.adminWelcomeWrapper}>
+          <StaticImage
+            src="../../images/dashboard/admin-avatar.png"
+            placeholder="blurred"
+            width={50}
+            alt="admin-avatar"
+          />
+          <p className={styles.welcomePhrase}>
+            <strong>Hi John,&nbsp;</strong>
+            here is the status of your hiring pipeline.
+          </p>
+        </div>
+        <div className={styles.tabContainer}>
+          <Tab.Container defaultActiveKey="active">
+            <div className={styles.navSwitcher}>
+              <p className={styles.pageTitle}>Jobs</p>
+              <Nav>
+                <Nav.Item>
+                  <Nav.Link eventKey="active">
+                    Active
+                    <p className="info">7</p>
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="closed">
+                    Closed
+                    <p className="info">12</p>
+                  </Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </div>
+            <Tab.Content>
+              <Tab.Pane eventKey="active">
+                <JobsTab status="active" />
+              </Tab.Pane>
+              <Tab.Pane eventKey="closed">
+                <JobsTab status="closed" />
+              </Tab.Pane>
+            </Tab.Content>
+
+          </Tab.Container>
+        </div>
+      </div>
     </AppLayout>
   );
 }
